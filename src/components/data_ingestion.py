6d44d0315700d77e58b8e79b944d7536ff94757a
shakti-sarada <shaktisahoo65@gmail.com>
2023-03-26 17:12:22 +0530

    Data Transformation File

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,6 +4,10 @@ from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
@@ -46,4 +50,7 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
--- src/exception.py
+++ src/exception.py
@@ -5,7 +5,7 @@ def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
     error_message="Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
-    file_name,exc_tb.tb_lineno,str(error))
+        file_name,exc_tb.tb_lineno,str(error))
 
     return error_message
 
