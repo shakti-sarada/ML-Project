afd06f7d8ae145aebf6fec58818988bf8265520a
shakti-sarada <shaktisahoo65@gmail.com>
2023-03-29 00:01:42 +0530

    Deployment Configuration

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -60,4 +60,4 @@ if __name__ == "__main__":
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
     modeltrainer=ModelTrainer()
-    print(modeltrainer.initate_model_trainer(train_arr,test_arr))
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
